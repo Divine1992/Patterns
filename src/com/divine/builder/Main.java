0ded5b5e8e44f575978016b68a9cfea1891c1506
divine <mefistofell081992@gmail.com>
2018-03-09 22:43:15 +0200

    change style

--- src/com/divine/builder/Main.java
+++ src/com/divine/builder/Main.java
@@ -2,33 +2,21 @@ package com.divine.builder;
 
 public class Main {
 
-	public static void main(String ...args) {
+	public static void main(String... args) {
 
-		//Custom builder
-		Person person = Person.newBuilder()
-							.name("John")
-							.age(10)
-							.build();
-		
+		// Custom builder
+		Person person = Person.newBuilder().name("John").age(10).build();
+
 		System.out.println(person);
-	
-		//Traditional builders
-		Account account = new Account.Builder()
-								.id(1)
-								.value(100)
-								.build();
-		
+
+		// Traditional builders
+		Account account = new Account.Builder().id(1).value(100).build();
+
 		System.out.println(account);
-		
-		Bank bank = new Bank.Builder()
-							.name("bank of America")
-							.username("Lampard")
-							.account(new Account.Builder()
-											.id(2)
-											.value(250)
-											.build())
-							.build();
-		
+
+		Bank bank = new Bank.Builder().name("bank of America").username("Lampard")
+				.account(new Account.Builder().id(2).value(250).build()).build();
+
 		System.out.println(bank);
 
 	}
